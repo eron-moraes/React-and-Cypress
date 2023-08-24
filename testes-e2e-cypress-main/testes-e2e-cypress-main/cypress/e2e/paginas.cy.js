a3bda9eb1e3775619eefaf06b0a8fa9efa744618
Eron Moraes <eron.moraes7@gmail.com>
2023-08-24 19:21:12 -0300

    jornadaUsuario

--- testes-e2e-cypress-main/testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
+++ testes-e2e-cypress-main/testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
@@ -1,19 +1,19 @@
 describe('Testando múltiplas páginas', () => { 
-    it('Deve conseguir acessar a página de cartões', ()=>{
-        cy.visit('http://localhost:3000/')
-      cy.getByData('botao-login').click()
-      cy.getByData('email-input').type('neilton@alura.com')
-      cy.getByData('senha-input').type('123456')
-      cy.getByData('botao-enviar').click()
-      cy.getByData('app-home').find('a').eq(1).click()
-      cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
-  
-    })
-   })
-    
-    
-    
-    
+  it('Deve conseguir acessar a página de cartões', ()=>{
+    cy.visit('http://localhost:3000/')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+
+    cy.location('pathname').should('eq','/home')
+
+    cy.getByData('app-home').find('a').eq(1).click()
+    cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+
+    cy.location('pathname').should('eq', '/home/cartoes')
+  })
+})
     
     
     
